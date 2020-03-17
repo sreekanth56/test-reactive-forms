d76b89d36b355e40d34e3b225cc25b93a8dc2965
sreekanth56 <koye.sree@gmail.com>
2020-03-17 18:40:37 +0530

    basic html design in reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-reactive-forms';
+
+  genders = ['male', 'female'];
+
 }
